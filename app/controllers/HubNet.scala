73a3142846dd34bc942b17889ec6eea91afc4737
TheBizzle <jason.bertsche@gmail.com>
2012-06-07 17:56:22 -0500

    Bug fixes, commentary, indentation, and frustration

--- app/controllers/HubNet.scala
+++ app/controllers/HubNet.scala
@@ -59,9 +59,9 @@ object HubNet extends Controller {
           import HubNetSettings._
           val vals  = Seq(modelName,    userName,    teacherName,    portNumber, isHeadless,    isLogging)
           val keys  = Seq(ModelNameKey, UserNameKey, TeacherNameKey, PortNumKey, IsHeadlessKey, IsLoggingKey)
-          val pairs = vals zip keys
+          val pairs = keys zip vals
           val encryptedMaybe = encryptHubNetInfoPairs(Map(pairs: _*))
-          encryptedMaybe fold ((ExpectationFailed(_)), (str => Redirect(routes.HubNet.hubSnoop(java.net.URLEncoder.encode(str, "UTF-8")))))
+          encryptedMaybe fold ((ExpectationFailed(_)), (str => Redirect(routes.HubNet.hubSnoop(str))))
           // Fail or redirect to snoop the IP
       }
     )
@@ -100,65 +100,68 @@ object HubNet extends Controller {
     Ok(views.html.hubsnoop(encryptedInfo))
   }
 
+  // Encoding and decoding is working properly here (which isn't all that surprising to me)
+  //@ We should just use POSTs, instead
   def handleTeacherProxy(encryptedStr: String, teacherIP: String) = Action {
-    request => handleHubNet(Success(java.net.URLDecoder.decode(encryptedStr, "UTF-8")), true, Option(teacherIP))(request)
+    request => handleHubNet(Success(encryptedStr), true, Option(teacherIP))(request)
   }
 
   def handleHubNet(encryptedStrMaybe: Validation[String, String], isTeacher: Boolean, teacherIP: Option[String] = None)
                   (implicit request: Request[AnyContent]) : Result = {
 
-      val inputAndSettingsMaybe = encryptedStrMaybe flatMap (input => DecryptionUtil.decodeForHubNet(input) map (settings => (input, settings)))
+    // Could this benefit from using a `for` comprehension?
+    val inputAndSettingsMaybe = encryptedStrMaybe flatMap (input => DecryptionUtil.decodeForHubNet(input) map (settings => (input, settings)))
 
-      inputAndSettingsMaybe flatMap {
-        case (input, HubNetSettings(modelNameOpt, username, isHeadless, teacherName, preferredPortOpt, isLogging)) =>
+    inputAndSettingsMaybe flatMap {
+      case (input, HubNetSettings(modelNameOpt, username, isHeadless, teacherName, preferredPortOpt, isLogging)) =>
 
-          val ipPortMaybe = {
-            import HubNetServerManager._
-            if (isTeacher) {
-              if (isHeadless)
-                startUpServer(modelNameOpt, teacherName, thisIP)
-              else
-                registerTeacherIPAndPort(teacherName, teacherIP.get, preferredPortOpt)
-            }
+        val ipPortMaybe = {
+          import HubNetServerManager._
+          if (isTeacher) {
+            if (isHeadless)
+              startUpServer(modelNameOpt, teacherName, thisIP)
             else
-              getPortByTeacherName(teacherName)
+              registerTeacherIPAndPort(teacherName, teacherIP.get, preferredPortOpt)
           }
+          else
+            getPortByTeacherName(teacherName)
+        }
 
-          val host = "http://" + request.host
-          val programName = modelNameOpt getOrElse "NetLogo"
-          val fileName = TempGenManager.formatFilePath(input, "jnlp")
-          val clientOrServerStr = if (!isHeadless && isTeacher) "Server" else "Client"
-          val (mainClass, argsMaybe) = {
-            if (isTeacher && !isHeadless)
-              ("org.nlogo.app.App", modelNameOpt map
-                                    (modelName => Seq("--url", ModelUtil.getURLFromName(modelName)) ++
-                                                  ipPortMaybe.fold({_ => Seq()}, { case (_, port) => Seq("--port", port.toString) }) ++
-                                                  (if (isLogging) Seq("--logging") else Seq())) map
-                                    (Success(_)) getOrElse Failure("No model name supplied."))
-            else
-              ("org.nlogo.hubnet.client.App", ipPortMaybe map { case (ip, port) => Seq("--id", username, "--ip", ip, "--port", port.toString) })
-          }
+        val host = "http://" + request.host
+        val programName = modelNameOpt getOrElse "NetLogo"
+        val fileName = TempGenManager.formatFilePath(input, "jnlp")
+        val clientOrServerStr = if (!isHeadless && isTeacher) "Server" else "Client"
+        val (mainClass, argsMaybe) = {
+          if (isTeacher && !isHeadless)
+            ("org.nlogo.app.App", modelNameOpt map
+                                  (modelName => Seq("--url", ModelUtil.getURLFromName(modelName)) ++
+                                                ipPortMaybe.fold({_ => Seq()}, { case (_, port) => Seq("--port", port.toString) }) ++
+                                                (if (isLogging) Seq("--logging") else Seq())) map
+                                  (Success(_)) getOrElse Failure("No model name supplied."))
+          else
+            ("org.nlogo.hubnet.client.App", ipPortMaybe map { case (ip, port) => Seq("--id", username, "--ip", ip, "--port", port.toString) })
+        }
 
-          val propsMaybe = argsMaybe map {
-            args => JNLP(
-              serverPublicURI  = new URI(host),
-              jnlpLoc          = fileName,
-              mainJar          = new MainJar("NetLogo.jar"),
-              applicationName  = "%s HubNet %s".format(programName, clientOrServerStr),
-              mainClass        = mainClass,
-              appTitle         = "NetLogo HubNet %s".format(clientOrServerStr),
-              desc             = "A HubNet %s for %s".format(clientOrServerStr.toLowerCase, programName),
-              shortDesc        = "HubNet (%s)".format(programName),
-              isOfflineAllowed = false,
-              otherJars        = if (isLogging) Seq(new Jar("logging.jar", true)) else Seq(),
-              properties       = if (isLogging) Seq(("jnlp.connectpath", "http://abmplus.tech.northwestern.edu:9001/logging")) else Seq(),
-              arguments        = args
-            )
-          }
+        val propsMaybe = argsMaybe map {
+          args => JNLP(
+            serverPublicURI  = new URI(host),
+            jnlpLoc          = fileName,
+            mainJar          = new MainJar("NetLogo.jar"),
+            applicationName  = "%s HubNet %s".format(programName, clientOrServerStr),
+            mainClass        = mainClass,
+            appTitle         = "NetLogo HubNet %s".format(clientOrServerStr),
+            desc             = "A HubNet %s for %s".format(clientOrServerStr.toLowerCase, programName),
+            shortDesc        = "HubNet (%s)".format(programName),
+            isOfflineAllowed = false,
+            otherJars        = if (isLogging) Seq(new Jar("logging.jar", true)) else Seq(),
+            properties       = if (isLogging) Seq(("jnlp.connectpath", "http://abmplus.tech.northwestern.edu:9001/logging")) else Seq(),
+            arguments        = args
+          )
+        }
 
-          propsMaybe map (jnlp => TempGenManager.registerFile(jnlp.toXMLStr, fileName).toString replaceAllLiterally("\\", "/"))
+        propsMaybe map (jnlp => TempGenManager.registerFile(jnlp.toXMLStr, fileName).toString replaceAllLiterally("\\", "/"))
 
-      } fold ((ExpectationFailed(_)), (url => Redirect("/" + url)))
+    } fold ((ExpectationFailed(_)), (url => Redirect("/" + url)))
 
   }
 
