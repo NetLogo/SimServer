711dc4a64e0a1756d961b7bdbbc15a79b4ac8384
TheBizzle <jason.bertsche@gmail.com>
2012-09-13 17:25:24 -0500

    Fixing issue with cross-site POSTing not working
    'Exploratory' OPTIONS requests are now responded to as 'APIAction's

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,6 +10,12 @@ import models.util.{ PlayUtil, Util }
 
 object Application extends Controller {
 
+  /*
+   "Wow!  WHAT IS THIS?!", you ask.  Well, let me tell you...
+   First, look into the HTTP request type OPTIONS.  Then, understand that this simply replies to the OPTIONS request,
+   saying that we'll accept pretty much any request--cross-domain, or not
+   */
+  def options(nonsense: String) = APIAction { Ok }
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
