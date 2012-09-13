aff3d0f99014dfd384cc9d5e88969384bde18625
TheBizzle <jason.bertsche@gmail.com>
2012-09-13 17:21:43 -0500

    Improving [APIAction]'s killing power

--- app/controllers/APIAction.scala
+++ app/controllers/APIAction.scala
@@ -11,9 +11,13 @@ import play.api.mvc.{ Action, AnyContent, BodyParser, Request, SimpleResult }
 
 private[controllers] object APIAction {
 
-  private val APIHeader = "Access-Control-Allow-Origin" -> "*"
+  private val APIDomainsHeader  = "Access-Control-Allow-Origin"  -> "*"
+  private val APIReqTypesHeader = "Access-Control-Allow-Methods" -> "POST, GET, OPTIONS, PUT, DELETE"
+  private val APIControlsHeader = "Access-Control-Allow-Headers" -> "Content-Type"
 
-  private val result2apiResult = (result: SimpleResult[_]) => result.withHeaders(APIHeader)
+  private val APIHeaders = Seq(APIDomainsHeader, APIReqTypesHeader, APIControlsHeader)
+
+  private val result2apiResult = (result: SimpleResult[_]) => result.withHeaders(APIHeaders: _*)
 
   def apply[A](bodyParser: BodyParser[A])(block: Request[A] => SimpleResult[_]) = Action(bodyParser)(block andThen result2apiResult)
   def apply(block: Request[AnyContent] => SimpleResult[_])                      = Action(block andThen result2apiResult)
