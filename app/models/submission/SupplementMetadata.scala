0e64c6daeb5be7c28d7935b3b7b7a28a558fa762
TheBizzle <jason.bertsche@gmail.com>
2012-10-25 18:14:34 -0500

    Moving the metadata parser into the new 'parse.submssion' package

--- app/models/submission/SupplementMetadata.scala
+++ app/models/submission/SupplementMetadata.scala
@@ -1,7 +1,5 @@
 package models.submission
 
-import play.api.libs.json.Json
-
 /**
  * Created with IntelliJ IDEA.
  * User: Jason
@@ -14,8 +12,3 @@ trait SupplementMetadata {
   def getType: String
 }
 
-object SupplementMetadataParser {
-  def apply(str: String) : SupplementMetadata = {
-    new SupplementMetadata { override def getType = (Json.parse(str) \ "type").as[String] }
-  }
-}
