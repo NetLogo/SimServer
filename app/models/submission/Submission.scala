5a37ddd2f6d3a5c369a06eeefbe1d7f4d1ce0854
TheBizzle <jason.bertsche@gmail.com>
2012-10-30 14:20:25 -0500

    Scolding myself

--- app/models/submission/Submission.scala
+++ app/models/submission/Submission.scala
@@ -17,6 +17,10 @@ trait Association extends Submission {
 
 trait Entry extends Submission
 
+//@ Umm... so let me get this straight, Mr. Jason...
+//  It _is_ alright for something to know that it's presentable
+//  But it's _not_ alright for something to actually know _how_ to present itself?
+//  ...THAT DON'T MAKE NO SENSE!!!!!!  Just give this trait an abstract `present` method and be done with it!!! --JAB (10/30/12)
 trait Presentable {
   def typ: String
 }
