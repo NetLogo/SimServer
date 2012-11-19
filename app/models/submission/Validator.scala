778c93343f648a353d2aa49cf53b2ae70dfe4120
TheBizzle <jason.bertsche@gmail.com>
2012-11-19 17:02:26 -0600

    Cleaning up/generalizing validation helpers

--- app/models/submission/Validator.scala
+++ app/models/submission/Validator.scala
@@ -11,26 +11,36 @@ import scalaz.{ Scalaz, ValidationNEL }, Scalaz.ToValidationV
 
 private[submission] object Validator {
 
-  def validateRefID(refID: String) : ValidationNEL[String, Long] =
-    failUnderCond(refID, StringEmptyCond, "Invalid value given for ref ID; ref ID cannot be empty") flatMap {
+  protected type V[T] = ValidationNEL[String, T]
+
+  def validateRefID(refID: String) : V[Long] =
+    ensureNotEmpty(refID, "ref ID") flatMap {
       x =>
-        try {
-          x.toLong.successNel
-        }
+        try x.toLong.successNel
         catch {
           case ex: NumberFormatException => "Cannot convert '%s' to Long; either not numerical or too many digits.".format(x).failNel
         }
     }
 
-  def validateTimestamp(timestamp: Long) = failUnderCond(timestamp, LongLTEZeroCond, "Invalid timestamp; value is too small")
-  def validatePeriodID(periodID: String) = failUnderCond(periodID,  StringEmptyCond, "Invalid period ID; period ID cannot be empty")
-  def validateRunID      (runID: String) = failUnderCond(runID,     StringEmptyCond, "Invalid run ID; run ID cannot be empty")
-  def validateUserID    (userID: String) = failUnderCond(userID,    StringEmptyCond, "Invalid username; username cannot be empty")
+  def validateTimestamp(timestamp: Long) = ensureNonNegative(timestamp, "timestamp")
+  def validateUserID    (userID: String) = ensureNotEmpty(userID,   "user_id")
+  def validatePeriodID(periodID: String) = ensureNotEmpty(periodID, "period id")
+  def validateRunID      (runID: String) = ensureNotEmpty(runID,    "run id")
 
-  protected val StringEmptyCond = (_: String).isEmpty
-  protected val LongLTEZeroCond = (_: Long) <= 0
+  protected val ErrorMessageTemplate = "Invalid value given for %s; %s".format(_: String, _: String)
 
-  protected def failUnderCond[T](param: T, cond: (T) => Boolean, errorStr: String) : ValidationNEL[String, T] = param.successNel[String] flatMap {
+  def ensure[T](data: T, dataName: String)(errorDesc: String)(errorCond: (T) => Boolean) : V[T] = {
+    lazy val errorMessage = ErrorMessageTemplate(dataName, errorDesc)
+    failUnderCond(data, errorCond, errorMessage)
+  }
+
+  def ensureNotEmpty[T <% { def isEmpty : Boolean }](data: T, dataName: String) : V[T] =
+    ensure(data, dataName)("cannot be empty")(_.isEmpty)
+
+  def ensureNonNegative[T <% AnyVal { def <=(x: Int) : Boolean }](data: T, dataName: String) : V[T] =
+    ensure(data, dataName)("value is too small")(_ <= 0)
+
+  protected def failUnderCond[T](param: T, cond: (T) => Boolean, errorStr: => String) : V[T] = param match {
     case x if cond(x) => errorStr.failNel
     case x            => x.successNel
   }
