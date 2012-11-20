8478682a7feec926613eea28378c9cffcd845f79
TheBizzle <jason.bertsche@gmail.com>
2012-11-20 12:42:25 -0600

    Making use of constants when referring to DB column names

--- app/models/submission/SubmissionDBManager.scala
+++ app/models/submission/SubmissionDBManager.scala
@@ -15,42 +15,42 @@ import scalaz.{ Scalaz, ValidationNEL }, Scalaz.ToValidationV
 
 import play.api.Play.current
 
-//@ Constantify field names
-
 object SubmissionDBManager {
 
   import AnormExtras._
 
   def getStudentsByRunAndPeriod(runID: String, periodID: String) : Seq[String] = {
     DB.withConnection { implicit connect =>
+      import DBConstants.UserWork._
       SQL (
         """
-          |SELECT DISTINCT user_id FROM user_work
-          |WHERE run_id = {run_id} AND period_id = {period_id};
-        """.stripMargin
+          |SELECT DISTINCT %s FROM %s
+          |WHERE %s = {run_id} AND %s = {period_id};
+        """.stripMargin.format(UserIDKey, TableName, RunIDKey, PeriodIDKey) //@ Do want string interpolation!
       ) on (
         "run_id"    -> runID,
         "period_id" -> periodID
         ) as {
-        str("user_id") map { identity } *
+        str(UserIDKey) map { identity } *
       }
     }
   }
 
   def getUserWork(period: String, run: String, user: String) : Seq[UserWork] = {
     DB.withConnection { implicit connection =>
+      import DBConstants.UserWork._
       SQL (
         """
-          SELECT * FROM user_work
-          WHERE period_id = {period} AND run_id = {run} AND user_id = {user};
-        """
+          |SELECT * FROM %s
+          |WHERE %s = {period} AND %s = {run} AND %s = {user};
+        """.stripMargin.format(TableName, PeriodIDKey, RunIDKey, UserIDKey)
       ) on (
         "period"    -> period,
         "run"       -> run,
         "user"      -> user
       ) as {
-        long("id") ~ timestamp("timestamp") ~ str("period_id") ~ str("run_id") ~ str("user_id") ~
-          str("type") ~ str("data") ~ str("metadata") ~ str("description") map {
+        long(IDKey) ~ timestamp(TimestampKey) ~ str(PeriodIDKey) ~ str(RunIDKey) ~ str(UserIDKey) ~
+          str(TypeKey) ~ str(DataKey) ~ str(MetadataKey) ~ str(DescriptionKey) map {
           case id ~ timestamp ~ session ~ run ~ user ~ typ ~ data ~ metadata ~ description =>
             UserWork(Option(id), timestamp, session, run, user, typ, data, metadata, description,
                      getWorkSupplementsByRefID(id), getWorkCommentsByRefID(id))
@@ -62,15 +62,16 @@ object SubmissionDBManager {
 
   def getWorkCommentsByRefID(workRefID: Long) : Seq[UserWorkComment] = {
     DB.withConnection { implicit connection =>
+      import DBConstants.UserWorkComments._
       SQL (
         """
-          SELECT * FROM user_work_comments
-          WHERE ref_id = {refID};
-        """
+          |SELECT * FROM %s
+          |WHERE %s = {refID};
+        """.stripMargin.format(TableName, RefIDKey)
       ) on (
         "refID" -> workRefID
       ) as {
-        long("id") ~ long("ref_id") ~ timestamp("timestamp") ~ str("user_id") ~ str("comment") map {
+        long(IDKey) ~ long(RefIDKey) ~ timestamp(TimestampKey) ~ str(UserIDKey) ~ str(CommentKey) map {
           case id ~ refID ~ timestamp ~ user ~ comment => UserWorkComment(Option(id), Option(refID), timestamp, user, comment)
           case _ => raiseDBAccessException
         } *
@@ -80,15 +81,16 @@ object SubmissionDBManager {
 
   def getWorkSupplementsByRefID(workRefID: Long) : Seq[UserWorkSupplement] = {
     DB.withConnection { implicit connection =>
+      import DBConstants.UserWorkSupplements._
       SQL (
         """
-          SELECT * FROM user_work_supplements
-          WHERE ref_id = {refID};
-        """
+          |SELECT * FROM %s
+          |WHERE %s = {refID};
+        """.stripMargin.format(TableName, RefIDKey)
       ) on (
         "refID" -> workRefID
       ) as {
-        long("id") ~ long("ref_id") ~ str("type") ~ str("data") ~ str("metadata") map {
+        long(IDKey) ~ long(RefIDKey) ~ str(TypeKey) ~ str(DataKey) ~ str(MetadataKey) map {
           case id ~ refID ~ typ ~ data ~ metadata => UserWorkSupplement(Option(id), Option(refID), typ, data, metadata)
           case _ => raiseDBAccessException
         } *
@@ -98,15 +100,16 @@ object SubmissionDBManager {
 
   def getTypeBundleByName(name: String) : ValidationNEL[String, TypeBundle] = {
     DB.withConnection { implicit connection =>
+      import DBConstants.TypeBundles._
       val opt = SQL (
         """
-          SELECT * FROM type_bundles
-          WHERE name = {name};
-        """
+          |SELECT * FROM %s
+          |WHERE %s = {name};
+        """.stripMargin.format(TableName, NameKey)
       ) on (
         "name" -> name
       ) as {
-        str("name") ~ str("action_js") ~ str("presentation_js") ~ str("file_extension") map {
+        str(NameKey) ~ str(ActionJSKey) ~ str(PresentationJSKey) ~ str(FileExtensionKey) map {
           case name ~ action ~ presentation ~ ext => TypeBundle(name, action, presentation, ext)
           case _                                  => raiseDBAccessException
         } *
@@ -129,24 +132,25 @@ private object Submittable {
   implicit def userWork2Submittable(userWork: UserWork) = new Submittable {
     override def submit : ValidationNEL[String, Long] = DB.withConnection { implicit connection =>
 
-        val sql = SQL (
-          """
-            INSERT INTO user_work
-            (timestamp, period_id, run_id, user_id, type, data, metadata, description) VALUES
-            ({timestamp}, {periodID}, {runID}, {userID}, {type}, {data}, {metadata}, {description});
-          """
-        ) on (
-          "timestamp"   -> userWork.timestamp,
-          "periodID"    -> userWork.periodID,
-          "runID"       -> userWork.runID,
-          "userID"      -> userWork.userID,
-          "type"        -> userWork.typ,
-          "data"        -> userWork.data,
-          "metadata"    -> userWork.metadata,
-          "description" -> userWork.description
-          )
+      import DBConstants.UserWork._
+      val sql = SQL (
+        """
+          |INSERT INTO %s
+          |(%s, %s, %s, %s, %s, %s, %s, %s) VALUES
+          |({timestamp}, {periodID}, {runID}, {userID}, {type}, {data}, {metadata}, {description});
+        """.stripMargin.format(TableName, TimestampKey, PeriodIDKey, RunIDKey, UserIDKey, TypeKey, DataKey, MetadataKey, DescriptionKey)
+      ) on (
+        "timestamp"   -> userWork.timestamp,
+        "periodID"    -> userWork.periodID,
+        "runID"       -> userWork.runID,
+        "userID"      -> userWork.userID,
+        "type"        -> userWork.typ,
+        "data"        -> userWork.data,
+        "metadata"    -> userWork.metadata,
+        "description" -> userWork.description
+        )
 
-        sql.executeInsert().get.successNel[String]
+      sql.executeInsert().get.successNel[String]
 
     }
   }
@@ -154,20 +158,21 @@ private object Submittable {
   implicit def workComment2Submittable(workComment: UserWorkComment) = new Submittable {
     override def submit : ValidationNEL[String, Long] = DB.withConnection { implicit connection =>
 
-        val sql = SQL (
-          """
-            INSERT INTO user_work_comments
-            (ref_id, timestamp, user_id, comment) VALUES
-            ({refID}, {timestamp}, {userID}, {comment});
-          """
-        ) on (
-          "refID"     -> workComment.refID,
-          "timestamp" -> workComment.timestamp,
-          "userID"    -> workComment.userID,
-          "comment"   -> workComment.comment
-        )
+      import DBConstants.UserWorkComments._
+      val sql = SQL (
+        """
+          |INSERT INTO %s
+          |(%s, %s, %s, %s) VALUES
+          |({refID}, {timestamp}, {userID}, {comment});
+        """.stripMargin.format(TableName, RefIDKey, TimestampKey, UserIDKey, CommentKey)
+      ) on (
+        "refID"     -> workComment.refID,
+        "timestamp" -> workComment.timestamp,
+        "userID"    -> workComment.userID,
+        "comment"   -> workComment.comment
+      )
 
-        sql.executeInsert().get.successNel[String]
+      sql.executeInsert().get.successNel[String]
 
     }
   }
@@ -175,20 +180,22 @@ private object Submittable {
   implicit def workSupplement2Submittable(workSupplement: UserWorkSupplement) = new Submittable {
     override def submit : ValidationNEL[String, Long] = DB.withConnection { implicit connection =>
 
-        val sql = SQL (
-          """
-            INSERT INTO user_work_supplements
-            (ref_id, type, data, metadata) VALUES
-            ({refID}, {type}, {data}, {metadata});
-          """
-        ) on (
-          "refID"    -> workSupplement.refID,
-          "type"     -> workSupplement.typ,
-          "data"     -> workSupplement.data,
-          "metadata" -> workSupplement.metadata
-        )
 
-        sql.executeInsert().get.successNel[String]
+      import DBConstants.UserWorkSupplements._
+      val sql = SQL (
+        """
+          |INSERT INTO %s
+          |(%s, %s, %s, %s) VALUES
+          |({refID}, {type}, {data}, {metadata});
+        """.stripMargin.format(TableName, RefIDKey, TypeKey, DataKey, MetadataKey)
+      ) on (
+        "refID"    -> workSupplement.refID,
+        "type"     -> workSupplement.typ,
+        "data"     -> workSupplement.data,
+        "metadata" -> workSupplement.metadata
+      )
+
+      sql.executeInsert().get.successNel[String]
 
     }
   }
@@ -196,12 +203,13 @@ private object Submittable {
   implicit def typeBundle2Submittable(bundle: TypeBundle) = new Submittable {
     override def submit : ValidationNEL[String, Long] = DB.withConnection { implicit connection =>
 
+      import DBConstants.TypeBundles._
       val sql = SQL (
         """
-          INSERT INTO type_bundles
-          (name, action_js, presentation_js, file_extension) VALUES
-          ({name}, {action_js}, {presentation_js}, {file_extension});
-        """
+          |INSERT INTO %s
+          |(%s, %s, %s, %s) VALUES
+          |({name}, {action_js}, {presentation_js}, {file_extension});
+        """.stripMargin.format(TableName, NameKey, ActionJSKey, PresentationJSKey, FileExtensionKey)
       ) on (
         "name"            -> bundle.name,
         "action_js"       -> bundle.actionJS,
@@ -226,13 +234,14 @@ private object Updatable {
   implicit def userWork2Updatable(userWork: UserWork) = new Updatable {
     override def update() { DB.withConnection { implicit connection =>
 
+      import DBConstants.UserWork._
       val sql = SQL (
         """
-          UPDATE user_work
-          SET timestamp={timestamp}, period_id={period_id}, run_id={run_id}, user_id={user_id},
-              type={type}, data={data}, metadata={metadata}, description={description}
-          WHERE id={id};
-        """
+          |UPDATE %s
+          |SET %s={timestamp}, %s={period_id}, %s={run_id}, %s={user_id},
+          |    %s={type}, %s={data}, %s={metadata}, %s={description}
+          |WHERE %s={id};
+        """.stripMargin.format(TableName, TimestampKey, PeriodIDKey, RunIDKey, UserIDKey, TypeKey, DataKey, MetadataKey, DescriptionKey, IDKey)
       ) on (
         "id"          -> userWork.id,
         "timestamp"   -> userWork.timestamp,
@@ -253,12 +262,13 @@ private object Updatable {
   implicit def workSupplement2Updatable(workSupplement: UserWorkSupplement) = new Updatable {
     override def update() { DB.withConnection { implicit connection =>
 
+      import DBConstants.UserWorkSupplements._
       val sql = SQL (
         """
-          UPDATE user_work_supplements
-          SET ref_id={ref_id}, type={type}, data={data}, metadata={metadata}
-          WHERE id={id};
-        """
+          |UPDATE %s
+          |SET %s={ref_id}, %s={type}, %s={data}, %s={metadata}
+          |WHERE %s={id};
+        """.stripMargin.format(TableName, RefIDKey, TypeKey, DataKey, MetadataKey, IDKey)
       ) on (
         "id"       -> workSupplement.id,
         "ref_id"   -> workSupplement.refID,
@@ -275,12 +285,13 @@ private object Updatable {
   implicit def typeBundle2Updatable(bundle: TypeBundle) = new Updatable {
     override def update() { DB.withConnection { implicit connection =>
 
+      import DBConstants.TypeBundles._
       val sql = SQL (
         """
-          UPDATE type_bundles
-          SET action_js={action_js}, presentation_js={presentation_js}, file_extension={file_extension}
-          WHERE name={name};
-        """
+          |UPDATE %s
+          |SET %s={action_js}, %s={presentation_js}, %s={file_extension}
+          |WHERE %s={name};
+        """.stripMargin.format(TableName, ActionJSKey, PresentationJSKey, FileExtensionKey, NameKey)
       ) on (
         "name"            -> bundle.name,
         "action_js"       -> bundle.actionJS,
@@ -299,4 +310,63 @@ object AnormExtras {
   import java.math.{ BigInteger => JBigInt }
   def timestamp(columnName: String) : RowParser[Long] = get[JBigInt](columnName)(implicitly[Column[JBigInt]]) map (new BigInt(_).toLong)
   def raiseDBAccessException = throw new java.sql.SQLException("Retrieved data from database in unexpected format.")
+}
+
+private object DBConstants {
+
+  trait Table {
+    def TableName: String
+  }
+
+  object TypeBundles extends Table {
+
+    override val TableName = "type_bundles"
+
+    val ActionJSKey       = "action_js"
+    val FileExtensionKey  = "file_extension"
+    val NameKey           = "name"
+    val PresentationJSKey = "presentation_js"
+
+  }
+
+  object UserWork extends Table {
+
+    override val TableName = "user_work"
+
+    val DataKey        = "data"
+    val DescriptionKey = "description"
+    val IDKey          = "id"
+    val MetadataKey    = "metadata"
+    val PeriodIDKey    = "period_id"
+    val RunIDKey       = "run_id"
+    val TimestampKey   = "timestamp"
+    val TypeKey        = "type"
+    val UserIDKey      = "user_id"
+
+  }
+
+  object UserWorkComments extends Table {
+
+    override val TableName = "user_work_comments"
+
+    val CommentKey   = "comment"
+    val IDKey        = "id"
+    val RefIDKey     = "ref_id"
+    val TimestampKey = "timestamp"
+    val UserIDKey    = "user_id"
+
+  }
+
+  object UserWorkSupplements extends Table {
+
+    override val TableName = "user_work_supplements"
+
+    val DataKey     = "data"
+    val IDKey       = "id"
+    val MetadataKey = "metadata"
+    val RefIDKey    = "ref_id"
+    val TypeKey     = "type"
+
+  }
+
 }
