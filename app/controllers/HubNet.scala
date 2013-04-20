79fc49d747a8dfb741f5bb8adb8ecd8b306c3b9e
TheBizzle <jason.bertsche@gmail.com>
2013-04-20 01:11:04 -0500

    Refactoring HubNet controller
    Most notably, separating the nasty JSON-generating code from the parameter-calculating code

--- app/controllers/HubNet.scala
+++ app/controllers/HubNet.scala
@@ -4,13 +4,16 @@ import
   play.api.mvc._
 
 import
-  scalaz.Scalaz,
+  scalaz.{ Scalaz, ValidationNel },
     Scalaz.ToValidationV
+
+import
+  play.api.libs.json.JsValue
 
 import
   models.{ hubnet, jnlp, util },
     hubnet.{ HubNetServerManager, StudentInfo, TeacherInfo },
-    jnlp._,
+    jnlp.Jar,
     util.{ HubNetSettings, Util }
 
 /**
@@ -60,6 +63,10 @@ object HubNet extends Controller {
 
   private def handleHubNet(params: Map[String, String], isTeacher: Boolean)(implicit request: Request[AnyContent]) : Result = {
 
+    import models.jnlp.{ HubNetJNLP, JNLPFromJSONGenerator, NetLogoJNLP }
+    import HubNetJNLP.{ generateAppName, generateDesc, generateIPArgs, generatePortArgs, generateShortDesc, generateUserIDArgs }
+    import NetLogoJNLP.generateLoggingArgs
+
     val HubNetDefaultPort = 9173
 
     val settingsMaybe = HubNetSettings(params, isTeacher) map (_.successNel[String]) getOrElse "Failed to interpret settings".failNel
@@ -78,15 +85,15 @@ object HubNet extends Controller {
         val programName = modelNameOpt getOrElse "NetLogo"
         val roleStr     = if (isTeacher) "Server" else "Client"
 
-        // Start setting values to go into JSON
-        import HubNetJNLP.{ generateAppName, generateDesc, generateShortDesc }
-
         val appName          = generateAppName(programName, roleStr)
         val desc             = generateDesc(programName, roleStr.toLowerCase)
         val shortDesc        = generateShortDesc(programName)
         val isOfflineAllowed = false
+        val modelURLOpt      = modelNameOpt map (Models.getHubNetModelURL(_))
+        val properties       = Util.ifFirstWrapSecond(isLogging, ("jnlp.connectpath", connectPath)).toSeq
+        val otherJars        = Seq()
+
         val args = {
-          import HubNetJNLP.{ generateIPArgs, generatePortArgs, generateUserIDArgs }, NetLogoJNLP.generateLoggingArgs
           if (isTeacher)
             ipPortMaybe.fold({_ => Seq()}, { case (_, port) => generatePortArgs(port) }) ++ generateLoggingArgs(isLogging)
           else
@@ -95,54 +102,61 @@ object HubNet extends Controller {
             } getOrElse Seq()
         }
 
-        // Building role-specific JSON
-        import play.api.libs.json.Json.{ toJson => js }
-
-        def propertyToJSON(property: (String, String)) = js(
-          Map(
-            JNLPKeys.PropertiesArrElemNameKey  -> js(property._1),
-            JNLPKeys.PropertiesArrElemValueKey -> js(property._2)
-          )
-        )
-
-        def jarToJSON(jar: Jar) = js(
-          Map(
-            JNLPKeys.OtherJarsArrElemNameKey   -> js(jar.jarName),
-            JNLPKeys.OtherJarsArrElemIsLazyKey -> js(jar.isLazy)
-          )
-        )
-
-        val properties        = Util.ifFirstWrapSecond(isLogging, ("jnlp.connectpath", connectPath)).toSeq
-        val otherJars         = Seq()
-        val roleExtrasMaybe   = {
-          if (!isTeacher) Map(HubNetKeys.IsHubNetClientKey -> js(true)).successNel[String]
-          else            modelNameOpt map (Models.getHubNetModelURL(_)) map {
-            modelURL =>
-              Map(
-                HubNetKeys.IsHubNetServerKey -> js(true),
-                NetLogoKeys.ModelURLKey      -> js(modelURL),
-                JNLPKeys.PropertiesKey       -> js(properties map propertyToJSON),
-                JNLPKeys.OtherJarsKey        -> js(otherJars  map jarToJSON)
-              ).successNel[String]
-          } getOrElse "No model name supplied".failNel
-        }
-
-        // Building general JSON; generating JNLP
-        roleExtrasMaybe flatMap {
-          extras =>
-            val json = js(
-              Map(
-                JNLPKeys.ApplicationNameKey  -> js(appName),
-                JNLPKeys.DescKey             -> js(desc),
-                JNLPKeys.ShortDescKey        -> js(shortDesc),
-                JNLPKeys.IsOfflineAllowedKey -> js(isOfflineAllowed),
-                JNLPKeys.ArgumentsKey        -> js(args)
-              ) ++ extras
-            )
-            JNLPFromJSONGenerator(json, request.host)
-        }
+        val jsonMaybe = paramsToJson(appName, desc, shortDesc, isOfflineAllowed, isTeacher, modelURLOpt, args, properties, otherJars)
+        jsonMaybe flatMap (JNLPFromJSONGenerator(_, request.host))
 
     } fold ((nel => ExpectationFailed(nel.list.mkString("\n"))), (url => Redirect("/" + url)))
+
+  }
+
+  private def paramsToJson(appName: String, desc: String, shortDesc: String, isOfflineAllowed: Boolean,  isTeacher: Boolean,
+                           modelURLOpt: Option[String], args: Seq[String], properties: Seq[(String, String)],
+                           otherJars: Seq[Jar]) : ValidationNel[String, JsValue] = {
+
+    import play.api.libs.json.Json.{ toJson => js }
+    import models.jnlp.{ HubNetKeys, Jar, JNLPKeys, NetLogoKeys }
+
+    def propertyToJSON(property: (String, String)) = js(
+      Map(
+        JNLPKeys.PropertiesArrElemNameKey  -> js(property._1),
+        JNLPKeys.PropertiesArrElemValueKey -> js(property._2)
+      )
+    )
+
+    def jarToJSON(jar: Jar) = js(
+      Map(
+        JNLPKeys.OtherJarsArrElemNameKey   -> js(jar.jarName),
+        JNLPKeys.OtherJarsArrElemIsLazyKey -> js(jar.isLazy)
+      )
+    )
+
+    val roleExtrasMaybe = {
+      if (!isTeacher)
+        Map(HubNetKeys.IsHubNetClientKey -> js(true)).successNel[String]
+      else
+        modelURLOpt map {
+          modelURL =>
+            Map(
+              HubNetKeys.IsHubNetServerKey -> js(true),
+              NetLogoKeys.ModelURLKey      -> js(modelURL),
+              JNLPKeys.PropertiesKey       -> js(properties map propertyToJSON),
+              JNLPKeys.OtherJarsKey        -> js(otherJars  map jarToJSON)
+            ).successNel[String]
+        } getOrElse "No model name supplied".failNel
+    }
+
+    roleExtrasMaybe map {
+      extras =>
+        js(
+          Map(
+            JNLPKeys.ApplicationNameKey  -> js(appName),
+            JNLPKeys.DescKey             -> js(desc),
+            JNLPKeys.ShortDescKey        -> js(shortDesc),
+            JNLPKeys.IsOfflineAllowedKey -> js(isOfflineAllowed),
+            JNLPKeys.ArgumentsKey        -> js(args)
+          ) ++ extras
+        )
+    }
 
   }
 
