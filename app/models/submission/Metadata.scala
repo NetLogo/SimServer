bd449542a06ef2b1eb45df232583d90260600e51
TheBizzle <jason.bertsche@gmail.com>
2013-12-02 22:13:33 -0600

    Making work metadata a bit easier to work with

--- app/models/submission/Metadata.scala
+++ app/models/submission/Metadata.scala
@@ -1,6 +1,10 @@
 package models.submission
 
-import play.api.libs.json.Json
+import
+  scala.util.Try
+
+import
+  play.api.libs.json.{ JsObject, Json }
 
 import
   scalaz.Scalaz.ToValidationV
@@ -14,16 +18,26 @@ import
 
 trait Metadata {
   def getType: String
+  def toMap:   Map[String, String]
+}
+
+class JsonMetadata(str: String) extends Metadata {
+  override def getType = (Json.parse(str) \ "type").as[String]
+  override def toMap   = Try(
+    Json.parse(str).asInstanceOf[JsObject].value.mapValues(_.toString).asInstanceOf[Map[String, String]]
+  ) getOrElse (
+    Map()
+  )
 }
 
 object Metadata extends FromStringParser {
   override protected type Target      = Metadata
   override protected type ConsTuple   = Nothing
   override protected type ParsedTuple = Nothing
-  override def constructFrom(parsed: Parsed)    = ??? // It just doesn't seem worth it in this trivial case to fully implement things
-  override def fromString(str: String): Output  = {
+  override def constructFrom(parsed: Parsed)   = ??? // It just doesn't seem worth it in this trivial case to fully implement things
+  override def fromString(str: String): Output = {
 
-    val metadata = new Metadata { override def getType = (Json.parse(str) \ "type").as[String] }
+    val metadata = new JsonMetadata(str)
 
     try {
       metadata.getType
