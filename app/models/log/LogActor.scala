2b2a94d6440c24347bcf2c4aa913354505be32c7
TheBizzle <jason.bertsche@gmail.com>
2013-12-05 18:15:56 -0600

    Drastically increasing log timeout

--- app/models/log/LogActor.scala
+++ app/models/log/LogActor.scala
@@ -22,7 +22,7 @@ class LogActor(id: Long, closeFunc: Long => Unit) extends Actor {
 
   import LogActorMessages._
 
-  context.setReceiveTimeout(60 seconds)
+  context.setReceiveTimeout(5 minutes)
 
   private val logFile = generateFile(id)
 
